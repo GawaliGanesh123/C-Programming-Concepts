27fdb7ea714add674c0aabf7965979b55fe10ac9
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2024-04-11 08:28:13 +0530

    Update Main.c

--- Main.c
+++ Main.c
@@ -4,13 +4,11 @@
 int main()
 {
     struct Demo obj;
-
     printf("Inside main\n");
     printf("Value of macro is : %d\n",MAX);
     printf("Size of structure is : %d\n",sizeof(obj));
 
     fun();
-
     return 0;
 }
 
