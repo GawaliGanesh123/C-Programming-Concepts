8adfc343a7351b10b862242075e328f25c5e7097
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2024-06-04 14:43:52 +0530

    Sequance.c

--- Sequance.c
+++ Sequance.c
@@ -9,10 +9,12 @@ int main()
     printf("Enter first number : \n");
     scanf("%d",&No1);
 
+
     printf("Enter second number : \n");
     scanf("%d",&No2);
 
     Ans = No1 + No2;
+
 
     printf("Addition is : %d\n",Ans);
 
