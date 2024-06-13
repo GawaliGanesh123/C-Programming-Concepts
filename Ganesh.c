a34e7bc054c7d9fbca81fd621ee488c0579c194f
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2024-06-13 06:56:08 +0530

    Add files via upload

---
+++ Ganesh.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Jay Ganesh ...\n");
+
+    return 0;
+}
+
+/*
+
+Programmer 
+
+Way of compile program
+    |
+Editor  -> Preprocessor -> Compiler -> Assembler -> Linker -> Loader -> OS -> Microprocessor
+
+*/
+
+// gcc Ganesh.c -o Myexe
